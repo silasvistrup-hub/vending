bcbbfd8319f2d9b1d9f390e36d164e04e37d10c7
Matthias Kristensen <matthias.r.kristensen@gmail.com>
2026-04-30 15:08:57 +0200

    Naming scheme fix

--- src/main/scala/VendingMachine.scala
+++ src/main/scala/VendingMachine.scala
@@ -14,55 +14,59 @@ class VendingMachine(maxCount: Int) extends Module {
     val tx = Output(Bool())
   })
 
+  /* Init DisplayMultiplexer */
   val DisplayMultiplexer = Module(new DisplayMultiplexer(maxCount))
 
+  /* Define states */
+  val inserted = RegInit(0.U(8.W)) // keeps track of inserted of coins not spent
+  val full2 = (inserted>97.U) // these two make sure not more than 99 is inserted
+  val full5 = (inserted>94.U)
+  val enough = (inserted>=io.price)
+  val countTo4 = RegInit(0.U(10.W))
+  val imFull = WireDefault(false.B)
 
-  val Value = RegInit(0.U(8.W)) // keeps track of value of coins not spent
-  val Full2 = (Value>97.U) // these two make sure not more than 99 is inserted
-  val Full5 = (Value>94.U)
-  val enough = (Value>=io.price)
-  val count_to_4 = RegInit(0.U(10.W))
-  val im_full = WireDefault(false.B)
+  /* Define input registers */
+  val prevCoin2 = RegNext(io.coin2)
+  val prevCoin5 = RegNext(io.coin5)
+  val prevPrice = RegNext(io.price)
+  val prevBuy   = RegNext(io.buy)
 
+  /* Define trigger */
+  val coin2Trigger = io.coin2 && !prevCoin2
+  val coin5Trigger = io.coin5 && !prevCoin5
+  val priceTrigger = io.price && !prevPrice
+  val buyTrigger   = io.buy && !prevBuy
 
-
-  val coin2_previous = RegNext(io.coin2)
-  val coin5_previous = RegNext(io.coin5)
-  val buy_previous   = RegNext(io.buy)
-
-  val coin2_change = io.coin2 && !coin2_previous
-  val coin5_change = io.coin5 && !coin5_previous
-  val buy_change   = io.buy && !buy_previous
-
+  /* Init SerialCommunicator module */
   val SerialComs = Module(new SerialCommunicator)
   SerialComs.io.price := io.price
-  SerialComs.io.sum := Value
-  SerialComs.io.update := coin2_change || coin5_change || buy_change
+  SerialComs.io.sum := inserted
+  SerialComs.io.update := coin2Trigger || coin5Trigger || priceTrigger || buyTrigger
   io.tx := SerialComs.io.tx
 
   when(coin2_change && !Full2) {
-    Value := Value + 2.U
+    inserted := inserted + 2.U
   }.elsewhen(coin5_change && !Full5) {
-    Value := Value + 5.U
+    inserted := inserted + 5.U
   }.elsewhen(buy_change && enough) {
-    Value := Value - io.price
+    inserted := inserted - io.price
   }
 
-  val ringalarm = RegInit(false.B)
+  val ringAlarm = RegInit(false.B)
   when((buy_change && !enough)){
-    ringalarm:=true.B
-    count_to_4 := 1.U
+    ringAlarm:=true.B
+    countTo4 := 1.U
   }
   val dispense = RegInit(false.B)
-  when(count_to_4 === 0.U) {
-    ringalarm := false.B
+  when(countTo4 === 0.U) {
+    ringAlarm := false.B
     dispense := false.B
   }
 
 
-  when((buy_change && enough)){
+  when((buyTrigger && enough)){
     dispense:=true.B
-  count_to_4 :=1.U
+    countTo4 :=1.U
   }
 
   def tickGenerator(max: Int): Bool = {
@@ -82,20 +86,20 @@ class VendingMachine(maxCount: Int) extends Module {
   val tick = tickGenerator(maxCount)
 
   when(tick) {
-    count_to_4 := count_to_4 + 1.U
+    countTo4 := countTo4 + 1.U
   }
 
 
-  when(Full2 && coin2_change){im_full:=true.B}
-  when(Full5 && coin5_change){im_full:=true.B}
+  when(full2 && coin2Trigger){imFull:=true.B}
+  when(full5 && coin5Trigger){imFull:=true.B}
 
 
 
-  io.alarm := ringalarm
+  io.alarm := ringAlarm
   io.releaseCan := dispense
-  DisplayMultiplexer.io.sum:=Value
+  DisplayMultiplexer.io.sum := inserted
   DisplayMultiplexer.io.price := io.price
-  DisplayMultiplexer.io.full := im_full
+  DisplayMultiplexer.io.full := imFull
   io.seg := DisplayMultiplexer.io.seg
   io.an := DisplayMultiplexer.io.an
 }
